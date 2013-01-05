20c188e73ad7378fffb53948b5362696f5e1b464
agustisanchez <agusti.sanchez@gmail.com>
2013-01-05 06:55:16 +0100

    package name, Maven

--- TomcatValve/src/main/java/com/geogrep/tomcat/valves/TenantValve.java
+++ TomcatValve/src/main/java/com/geogrep/tomcat/valves/TenantValve.java
@@ -1,4 +1,4 @@
-package tomcatvalve;
+package com.geogrep.tomcat.valves;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -10,11 +10,24 @@ import javax.servlet.ServletException;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
 import org.apache.catalina.valves.ValveBase;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 
-public class TestValve extends ValveBase {
+/**
+ * Emulates 3Scale plugin by adding a pre-configured tenant_id and api_key parameters to the request if not present.
+ * @author  Agustí Sánchez
+ *
+ */
+public class TenantValve extends ValveBase {
+
+    private Log logger = LogFactory.getLog(getClass());
 
     private Map<String, String> paramMap = new HashMap<>();
 
+    /**
+     * Initializes the parameter map with the parameters defined in server.xml.
+     * @param params a comma-delimited lists of requests parameters to add; i.e use commas instead of ampersands.
+     */
     public void setParams(String params) {
         StringTokenizer tokenizer = new StringTokenizer(params, ",");
         while (tokenizer.hasMoreTokens()) {
@@ -22,17 +35,18 @@ public class TestValve extends ValveBase {
             paramMap.put(paramTokenizer.nextToken(), paramTokenizer.hasMoreTokens() ? paramTokenizer.nextToken() : null);
         }
 
-        System.out.println("Initialized with " + paramMap);
-    }
-    
-    public void setRequestmatches(String matches)
-    {
-        
+        logger.info("Initialized with parameters" + paramMap);
     }
 
+    public void setRequestmatches(String matches) {
+
+    }
+
+    @SuppressWarnings("deprecation")
     @Override
     public void invoke(Request request, Response response) throws IOException, ServletException {
-        System.out.printf("Processing request %s for context %s with referrer %s.\n", request.getRequestURI(), request.getContextPath(), request.getHeader("referer"));
+        if (logger.isDebugEnabled())
+            logger.debug("Processing request \"" + request.getRequestURI() + "\".");
 
         for (String param : paramMap.keySet()) {
             if (request.getParameter(param) == null) {
